3a136a1e266515fb7de99876f3332849fc3ffbc4
Peyton <pekellogg@hotmail.com>
2022-03-15 17:50:53 -0700

    commit tree c6f8fe983dbadfb31d15511c1dd82a20eb06f756

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-    
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        input = params["user_phrase"]
+        @piglatinized_input = PigLatinizer.new.piglatinize(input)
+        erb :results
+    end
+
 end
